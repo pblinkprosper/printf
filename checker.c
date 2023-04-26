b15c055956a850dc5cb9871817ce2338324d7091
promkingh <iampromiseds@gmail.com>
2023-04-26 05:47:34 -0700

    Updated Codes

--- checker.c
+++ checker.c
@@ -9,31 +9,37 @@
  */
 int checker(const char *format, va_list args)
 {
-	int i, symbs, len, (*func)(va_list);
+	int i, j, symbs, len;
+	check_t s_symbs[] = {{"c", _putchar}, {"s", _puts}, {"d", _printint},
+		{"i", _printint}, {"%", _printpercent}, {NULL, NULL}};
 
 	symbs = 0;
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
-			func = get_format(&format[i + 1]);
-			if (func != NULL)
+			for (j = 0; s_symbs[j].sym != NULL; j++)
 			{
-				len = func(args);
-				if (len == -1)
-					return (-1);
-				symbs += len;
-				i++;
+				if (format[i + 1] == s_symbs[j].sym[0])
+				{
+					len = s_symbs[j].f(args);
+					if (len == -1)
+						return (-1);
+					symbs += len;
+					break;
+				}
 			}
-			else if (format[i + 1] && format[i + 1] != ' ')
+			if (s_symbs[j].sym == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1])
 				{
 					my_putchar(format[i]);
 					my_putchar(format[i + 1]);
 					symbs = symbs + 2;
-					i++;
 				}
+				else
+					return (-1);
+				i++;
 			}
 		}
 		else
@@ -44,34 +50,3 @@ int checker(const char *format, va_list args)
 	}
 	return (symbs);
 }
-
-/**
- * get_format - gets a functions based on the read format
- * @format: the string containing the arguments
- * Return: function
- */
-
-int (*get_format(const char *format))(va_list)
-{
-	int k;
-
-	check_t s_symbs[] = {{
-		"c", _putchar},
-		{"s", _puts},
-		{"d", _printint},
-		{"i", _printint},
-		{"%", _printpercent},
-		{NULL, NULL}
-	};
-
-	for (k = 0; s_symbs[k].sym; k++)
-	{
-		if (s_symbs[k].sym[0] == format[0])
-		{
-			return (s_symbs[k].f);
-		}
-	}
-
-	return (NULL);
-}
-
